864a2bf52da73971c9cf21dc5cd7c7f29ae3daa5
tkd-kaijo <takeda@kaijo.ed.jp>
2026-09-23 19:26:17 +0900

    Update sw.js

--- SanrioKart/sw.js
+++ SanrioKart/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sanrio-kart-v2-driver-view";
+const CACHE = "sanrio-kart-v3-wheel-control";
 const CORE = ["./", "./index.html", "./styles.css", "./game3d.js", "./manifest.webmanifest", "./assets/app-icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
